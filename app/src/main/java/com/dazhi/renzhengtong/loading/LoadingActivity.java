bbd89bd95e1e58c7f9baabad57477a298abdce8e
mac <mac@macdeiMac.local>
2018-01-24 18:03:53 +0800

    认证通

---
+++ app/src/main/java/com/dazhi/renzhengtong/loading/LoadingActivity.java
@@ -0,0 +1,54 @@
+package com.dazhi.renzhengtong.loading;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.LinearInterpolator;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.dazhi.renzhengtong.MainActivity;
+import com.dazhi.renzhengtong.R;
+
+/**
+ * Created by mac on 2018/1/22.
+ */
+
+public class LoadingActivity extends AppCompatActivity implements MyProgressView.OnProgressChangedListener {
+
+    private MyProgressView progressBar;
+    private TextView textView;
+    private int duration = 3000;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_loading);
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+        textView.setText(duration+"s");
+        progressBar.setListener(this);
+        progressBar.setDuration(duration);
+        progressBar.start();
+
+    }
+
+
+    @Override
+    public void onProgressChanged(int progress) {
+        if (progress == 0){
+            textView.setText("0s");
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            textView.setText((progress)*duration/1000/100+1+"s");
+        }
+    }
+}
